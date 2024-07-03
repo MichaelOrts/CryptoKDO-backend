dc6a667d65bf30f272b4fbf1d59505c90330b9cc
Michael Orts <michael.orts.86@gmail.com>
2024-07-03 16:45:39 +0200

    change network name localhost in hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,15 +5,23 @@ const ALCHEMY = process.env.ALCHEMY || "";
 const PK = process.env.PK || "0x00000000000000000000000000000000000000000000000000000000000000";
 const ETHERSCAN = process.env.ETHERSCAN || "";
 
+if(PK){
+  sepolia = {
+    url: ALCHEMY,
+    accounts: [`0x${PK}`],
+    chainId: 11155111
+  }
+} else{
+  sepolia = {
+    url: ALCHEMY,
+  }
+}
+
 module.exports = {
   solidity: "0.8.24",
   defaultNetwork: "hardhat",
   networks: {
-    sepolia: {
-      url: ALCHEMY,
-      accounts: [`0x${PK}`],
-      chainId: 11155111
-    },
+    sepolia: sepolia,
     hardhat: {
       chainId: 31337,
     }
