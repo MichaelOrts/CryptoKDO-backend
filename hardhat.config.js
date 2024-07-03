29cad6e58229cdaf4f06e2d176018383042845b0
Michael Orts <michael.orts.86@gmail.com>
2024-07-03 16:45:39 +0200

    change network name localhost in hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,7 +13,7 @@ module.exports = {
       accounts: [`0x${PK}`],
       chainId: 11155111
     },
-    localhost: {
+    hardhat: {
       url: "http://127.0.0.1:8545",
       chainId: 31337,
     }
