9be3e524876d980e6e0f42696a470f2fe7bc667c
Michael Orts <michael.orts.86@gmail.com>
2024-07-10 02:09:28 +0200

    update ignition module with new constructor and mock

--- ignition/modules/CryptoKDO.js
+++ ignition/modules/CryptoKDO.js
@@ -1,6 +1,9 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+const { ethers } = require('hardhat');
 
 module.exports = buildModule("CryptoKDOModule", (m) => {
-  const cryptoKDO = m.contract("CryptoKDO");
+  const erc20 = m.contract("ERC20Mock");
+  const wtg = m.contract("WrappedTokenGatewayMock", [erc20], {value: ethers.parseEther('1000')});
+  const cryptoKDO = m.contract("CryptoKDO", [wtg, erc20]);
   return { cryptoKDO };
 });
